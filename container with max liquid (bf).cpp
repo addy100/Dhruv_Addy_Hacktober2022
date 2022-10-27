e01f5175e6e18b25cb0a22989d48c65df1112a8a
addy100 <72308696+addy100@users.noreply.github.com>
2022-10-27 15:50:53 +0530

    container with max liquid

---
+++ container with max liquid (bf).cpp
@@ -0,0 +1,23 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxArea(vector<int> &numz)
+{
+    int mx = INT_MIN;
+	for(int i = 0; i < numz.size(); i++){
+		for(int j = i + 1; j < numz.size(); j++){
+			int liquid = (j-i)*min(numz[i], numz[j]);
+			mx = max(mx,liquid);
+		}
+	}
+    return mx;
+}
+
+int main() {
+    vector<int> numz {1, 4, 2, 3};
+  
+    cout << "Maximum amount of liquid is: " << maxArea(numz);
+  
+    return 0;
+}
