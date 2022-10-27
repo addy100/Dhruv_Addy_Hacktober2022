244be89691f0254bdfe4097c6aa19b4e7544b7d8
addy100 <72308696+addy100@users.noreply.github.com>
2022-10-28 01:36:15 +0530

    updated container with max liquid (optimised)

--- container with max liquid/container with max liquid (optimised).cpp
+++ container with max liquid/container with max liquid (optimised).cpp
@@ -1,15 +1,16 @@
-//optimised approach
+///container with max liquid
+///optimised approach
 #include<bits/stdc++.h>
 using namespace std;
 
 int maxArea(vector<int> &numz)
-{
+{ ///vector for calculating the max area 
     int i = 0,j = numz.size() - 1, mx = INT_MIN;
 	while(i < j)
 	{
 		int liquid = (j-i)*min(numz[i],numz[j]);
-		mx = max(mx,liquid);
-		if(numz[i] < numz[j])
+		mx = max(mx,liquid); 
+		if(numz[i] < numz[j]) 
 		    i++;
 		else
 		    j--;
@@ -21,7 +22,7 @@ int maxArea(vector<int> &numz)
 int main() {
     vector<int> numz {1, 4, 2, 3};
   
-    cout << "Maximum amount of liquid is: " << maxArea(numz);
+    cout << "Maximum amount of liquid is: " << maxArea(numz); //obtaining the max liquid quantity
   
     return 0;
 }
